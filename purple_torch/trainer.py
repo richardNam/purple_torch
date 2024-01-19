7e7c6ff99038e5fde651ffacd14ec64ddede9d11
Richard Nam <richardhnam@gmail.com>
2024-01-19 11:53:10 -0800

    add trainer TODOs

--- purple_torch/trainer.py
+++ purple_torch/trainer.py
@@ -4,7 +4,15 @@ from torch import nn
 
 
 class Trainer:
-    """A base NN trainer."""
+    """A base NN trainer.
+    Things to add to this module:
+    - Scheduler
+    - Evalution
+        - plots (loss over batch and epoch)
+        - metrics
+        - ...
+
+    """
 
     def __init__(self):
         pass
